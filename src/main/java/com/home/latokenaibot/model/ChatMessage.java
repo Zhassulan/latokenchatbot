e55e1c49f2e81012db0a7771522ba63d9306209a
zhass <developer.kz@gmail.com>
2024-11-15 14:47:09 +0500

    Chat bot

--- src/main/java/com/home/latokenaibot/model/ChatMessage.java
+++ src/main/java/com/home/latokenaibot/model/ChatMessage.java
@@ -1,10 +1,14 @@
 package com.home.latokenaibot.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
     private String message;
 }
